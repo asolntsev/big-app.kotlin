c06d1bf110e5fc919c1dbc237cec50c856902079
Andrei Solntsev <andrei.solntsev@gmail.com>
2017-08-31 23:42:12 +0300

    initial version

---
+++ src/main/kotlin/controllers2/Hello1111111111111111.kt
@@ -0,0 +1,664 @@
+package controllers2
+
+import util.getSomething
+import java.util.*
+
+object Hello1111111111111111 {
+
+    fun hello() {
+        println("Hello " + debug()!!)
+    }
+
+    private fun debug(): Any? {
+        return getSomething()
+    }
+
+    private fun sort(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort2(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort3(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort4(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort5(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort6(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort7(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort8(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort9(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort10(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort11(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort12(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort13(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort14(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort15(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort16(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort17(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort18(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort19(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort20(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort21(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort22(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort23(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort24(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort25(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort26(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort27(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort28(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort29(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort30(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort31(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort32(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort33(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort34(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort35(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort36(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort37(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort38(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort39(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort40(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort41(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort42(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort43(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort44(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort45(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort46(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort47(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort48(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort49(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    private fun sort50(list: List<Any>) {
+        Collections.sort(list) { o1, o2 -> o2.hashCode() - o2.hashCode() }
+    }
+
+    public fun sortAll(list: List<Any>) {
+        sort(list)
+        sort2(list)
+        sort3(list)
+        sort4(list)
+        sort5(list)
+        sort6(list)
+        sort7(list)
+        sort8(list)
+        sort9(list)
+        sort10(list)
+        sort11(list)
+        sort12(list)
+        sort13(list)
+        sort14(list)
+        sort15(list)
+        sort16(list)
+        sort17(list)
+        sort18(list)
+        sort19(list)
+        sort20(list)
+        sort21(list)
+        sort22(list)
+        sort23(list)
+        sort24(list)
+        sort25(list)
+        sort26(list)
+        sort27(list)
+        sort28(list)
+        sort29(list)
+        sort30(list)
+        sort31(list)
+        sort32(list)
+        sort33(list)
+        sort34(list)
+        sort35(list)
+        sort36(list)
+        sort37(list)
+        sort38(list)
+        sort39(list)
+        sort40(list)
+        sort41(list)
+        sort42(list)
+        sort43(list)
+        sort44(list)
+        sort45(list)
+        sort46(list)
+        sort47(list)
+        sort48(list)
+        sort49(list)
+        sort50(list)
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+        for (i in 0..9) {
+            sort50(list)
+        }
+    }
+}
---
+++ src/main/kotlin/util/Util.kt
@@ -0,0 +1,5 @@
+package util
+
+fun getSomething(): Int? {
+    return Integer.valueOf(42)
+}
